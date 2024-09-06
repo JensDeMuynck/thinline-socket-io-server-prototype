cad2a533d11b01a54aaba541858f5c359bfb9a5b
Jens De Muynck <jens@thinline.be>
2024-09-06 07:25:44 +0200

    Base IO server (attempt 2)

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const httpServer = createServer();
-const io = new Server(httpServer, { /* options */ });
-
-io.on("connection", (socket) => {
-	console.log(socket);
+const io = new Server(httpServer, {
+	cors: {
+		origin: "http://creem:8888"
+	}
 });
 
-httpServer.listen(3000);
+const connectedClients = new Map();
+
+httpServer.listen(3000, () => {
+	console.log('listening on *:3000');
+});
+
+io.on("connection", (socket) => {
+	socket.on('disconnect', function () {
+		connectedClients.delete(socket.id);
+
+		socket.broadcast.emit("other-socket-disconnected", socket.id);
+	});
+
+	socket.on('update-client-to-server', data => {
+		connectedClients.set(socket.id, data);
+		console.log(connectedClients);
+		socket.broadcast.emit("update-server-to-client", Array.from(connectedClients));
+	});
+});
